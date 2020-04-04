9cde7eb98e0aff1612c7f8591dd7d63da1024448
Savi <savannah.williams1623@gmail.com>
2020-04-04 11:58:58 -0600

    Working on router

---
+++ filmList/config/router.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+
+import Home from '../screens/Home.js';
+import MovieSeen from '../screens/MovieSeen.js';
+import MovieToSee from '../screens/MovieToSee.js';
+import Review from '../screens/Review.js';
+import SeenList from '../screens/SeenList.js';
+import ToSeeList from '../screens/ToSeeList';
+
+const Stack = createStackNavigator();
+
+function Root() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='SeenList' component={SeenList} />
+        <Stack.Screen name='ToSeeList' component={ToSeeList} />
+        <Stack.Screen name='MovieSeen' component={MovieSeen} />
+        <Stack.Screen name='MovieToSee' component={MovieToSee} />
+        <Stack.Screen name='Review' component={Review} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Root;
